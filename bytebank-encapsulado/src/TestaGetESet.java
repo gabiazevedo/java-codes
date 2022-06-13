cc1b7142bf51ea21bbcfa92ea6aa969a9981c45e
gabiazevedo <gabiazevedoms@gmail.com>
2022-06-13 17:33:26 -0300

    feat: Implementação dos métodos getters e setters - encapsulamento

---
+++ bytebank-encapsulado/src/TestaGetESet.java
@@ -0,0 +1,28 @@
+
+public class TestaGetESet {
+	
+	public static void main(String[] args) {
+		Conta conta = new Conta();
+		// conta.numero = 1245;
+		//essa linha não compila, porque agora o atributo número é um atributo privado da classe conta e só pode ser alterado através do método setNumero()
+		
+		conta.setNumero(1337);
+		System.out.println(conta.getNumero());
+		
+		Cliente paulo = new Cliente();
+		
+		paulo.setNome("Paulo Silveira");
+		conta.setTitular(paulo);
+		System.out.println(conta.getTitular().getNome());
+		
+		conta.getTitular().setProfissao("programador");
+		// em duas linhas:
+		Cliente titularDaConta = conta.getTitular();
+		titularDaConta.setProfissao("Programador");
+		
+		// esses prints demonstram o conceito de referência no Java. 
+		System.out.println(titularDaConta);
+		System.out.println(paulo);
+		System.out.println(conta.getTitular());
+	}
+}
